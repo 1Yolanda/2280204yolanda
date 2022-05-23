613f87ca6c41bdbdf2ff33e0dbeabd5ba12804a2
Yolanda Bermeo <yolandabermeotrujillo@gmail.com>
2022-05-22 22:08:07 -0500

    agregando cambios nuevos

---
+++ neflix/neflix.js
@@ -0,0 +1,10 @@
+function mensajeInsidiuos() {
+    alert('¿Ya te la viste esta peli :)?')
+}
+function Soul_portada() {
+    document.getElementById("imgPrincipal").scr = "img/Soul_portada.jpg";
+}
+function intercambiarPeliculas(ruta) {
+    document.getElementById("imgPrincipal").src = ruta;     
+
+}
